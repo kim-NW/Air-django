7c89acf332a495cfeb2de32518b10c2cb91feeb5
knw <kimnamwook08@gmail.com>
2023-04-03 18:08:27 +0900

    0403

--- categories/views.py
+++ categories/views.py
@@ -4,13 +4,63 @@ from django.core import serializers
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
 
 from .models import Category
+from .serializers import CategorySerializer
 
-@api_view()
-def categories(request):
-    return Response(
-        {
-            "ok": True,
-        }
-    )
+class Categories(APIView):
+    def get(self, request):
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            new_category = serializer.save()
+            CategorySerializer(new_category)
+            return Response(
+                CategorySerializer(new_category).data,
+            )
+        else:
+            return Response(serializer.errors)
+    
+
+class CategoryDetail(APIView):
+    
+    def get_object(self, pk):
+        try:
+            category = Category.objects.get(pk=pk)
+        except Category.DoseNotExist:
+            raise NotFound
+        return category
+
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        serializer = CategorySerializer(
+            self.get_object(pk),
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(
+                CategorySerializer(updated_category).data,
+            )
+        else:
+            return Response(serializer.errors)
+    
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
+
+
+        

db42292474e1f4d44807e4aebd68a310f84b0d47
knw <kimnamwook08@gmail.com>
2023-04-03 12:54:14 +0900

    0403

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Category
+
+@api_view()
+def categories(request):
+    return Response(
+        {
+            "ok": True,
+        }
+    )
